bd04a62b2ea6fbd5a2479c11b7af1b9dc43a42db
Vitalii Vorobii <vitalik.vorobij15@gmail.com>
2021-01-15 14:21:54 +0200

    Added exception handler

---
+++ src/main/java/com/file/converter/fileconverter/exception/handler/GeneralExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.file.converter.fileconverter.exception.handler;
+
+import com.file.converter.fileconverter.exception.FileFormatNotSupportedException;
+import com.file.converter.fileconverter.exception.FileProcessingException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GeneralExceptionHandler {
+
+    @ExceptionHandler(FileFormatNotSupportedException.class)
+    public ResponseEntity<String> handleFileFormatIsNotSupported(FileFormatNotSupportedException ignored) {
+        return new ResponseEntity<>("The provided file's format is not supported yet", HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(FileProcessingException.class)
+    public ResponseEntity<String> handleFileProcessingFailed(FileProcessingException ignored) {
+        return new ResponseEntity<>("Processing of the file failed", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
